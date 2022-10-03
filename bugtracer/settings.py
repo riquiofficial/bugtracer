530dc885abe663f96baa40937222ab1d4e3b3063
Richard <67551764+riquiofficial@users.noreply.github.com>
2022-10-03 08:39:44 +0100

    Turn debug mode off by default

--- bugtracer/settings.py
+++ bugtracer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DJANGO_DEBUG") != 'False'
+DEBUG = env.bool("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bug-tracer.herokuapp.com', 'bug-tracer.onrender.com']
 
