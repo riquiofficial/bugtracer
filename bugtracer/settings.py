9ab41a69f88442b022bbc2e3cf7cf69a9e173637
Richard <67551764+riquiofficial@users.noreply.github.com>
2022-10-03 08:38:51 +0100

    Added render to allowed_hosts

--- bugtracer/settings.py
+++ bugtracer/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG") != 'False'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bug-tracer.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bug-tracer.herokuapp.com', 'bug-tracer.onrender.com']
 
 
 # Application definition
